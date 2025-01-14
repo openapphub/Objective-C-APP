c48059b307d342de416dbf295e1ca4d81a6646a0
jinxiaoman <0027023149@iwhalecloud.com>
2025-01-14 15:20:30 +0800

    update: 初始化

---
+++ Tracker/Networking/APIClient.h
@@ -0,0 +1,16 @@
+//
+//  APIClient.h
+//  Tracker
+//
+//  Created by jin xm on 2025/1/14.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface APIClient : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Tracker/Networking/APIClient.m
@@ -0,0 +1,12 @@
+//
+//  APIClient.m
+//  Tracker
+//
+//  Created by jin xm on 2025/1/14.
+//
+
+#import "APIClient.h"
+
+@implementation APIClient
+
+@end
